aa3bae61f6407f19af509271794bb99e64c97cfa
thisisamandaliu <60411737+thisisamandaliu@users.noreply.github.com>
2020-02-07 08:37:16 -0800

    Create messages.js

---
+++ messages.js
@@ -0,0 +1,212 @@
+function setup() {
+  createCanvas(400, 700);
+}
+
+function printA(x, y) {
+  line(x, y, x, y + 100);
+  line(x + 40, y, x + 40, y + 100);
+  line(x, y + 50, x + 40, y + 50);
+  line(x, y, x + 40, y);
+}
+
+function printB(x, y) {
+  line(x + 10, y, x + 10, y + 100);
+  line(x + 40, y, x + 40, y + 100);
+  line(x + 10, y + 50, x + 40, y + 50);
+  line(x, y, x + 40, y);
+  line(x, y + 100, x + 40, y + 100);
+}
+
+function printC(x, y) {
+  line(x, y, x + 40, y);
+  line(x, y, x, y + 100);
+  line(x, y + 100, x + 40, y + 100);
+}
+
+function printD(x, y) {
+  line(x + 10, y, x + 10, y + 100);
+  line(x, y, x + 40, y);
+  line(x + 40, y, x + 40, y + 100);
+  line(x, y + 100, x + 40, y + 100);
+}
+
+function printE(x, y) {
+  line(x, y, x, y + 100);
+  line(x, y, x + 40, y);
+  line(x, y + 50, x + 40, y + 50);
+  line(x, y + 100, x + 40, y + 100);
+}
+
+function printF(x, y) {
+  line(x, y, x, y + 100);
+  line(x, y, x + 40, y);
+  line(x, y + 50, x + 40, y + 50);
+}
+
+function printG(x, y) {
+  line(x, y, x + 40, y);
+  line(x, y, x, y + 100);
+  line(x, y + 100, x + 40, y + 100);
+  line(x + 40, y + 50, x + 40, y + 100);
+  line(x + 30, y + 50, x + 40, y + 50);
+}
+
+function printH(x, y) {
+  line(x, y, x, y + 100);
+  line(x + 40, y, x + 40, y + 100);
+  line(x, y + 50, x + 40, y + 50);
+}
+
+function printI(x, y) {
+  line(x + 10, y, x + 30, y);
+  line(x + 10, y + 100, x + 30, y + 100);
+  line(x + 20, y, x + 20, y + 100);
+}
+
+function printJ(x, y) {
+  line(x + 20, y, x + 40, y);
+  line(x, y + 100, x + 40, y + 100);
+  line(x + 40, y, x + 40, y + 100);
+}
+
+function printK(x, y) {
+  line(x, y, x, y + 100);
+  line(x, y + 50, x + 40, y + 50);
+  line(x + 40, y, x + 40, y + 50);
+  line(x + 30, y + 50, x + 30, y + 100);
+  line(x + 30, y + 100, x + 40, y + 100);
+}
+
+function printL(x, y) {
+  line(x, y, x, y + 100);
+  line(x, y + 100, x + 40, y + 100);
+}
+
+function printM(x, y) {
+  line(x, y, x, y + 100);
+  line(x + 40, y, x + 40, y + 100);
+  line(x + 20, y, x + 20, y + 100);
+  line(x, y, x + 40, y);
+}
+
+function printN(x, y) {
+  line(x, y, x, y + 100);
+  line(x + 40, y, x + 40, y + 100);
+  line(x + 20, y, x + 20, y + 100);
+  line(x, y, x + 20, y);
+  line(x + 20, y + 100, x + 40, y + 100);
+}
+
+function printO(x, y) {
+  line(x, y, x, y + 100);
+  line(x, y, x + 40, y);
+  line(x + 40, y, x + 40, y + 100);
+  line(x, y + 100, x + 40, y + 100);
+}
+
+function printP(x, y) {
+  line(x, y, x, y + 100);
+  line(x + 40, y, x + 40, y + 50);
+  line(x, y + 50, x + 40, y + 50);
+  line(x, y, x + 40, y);
+}
+
+function printQ(x, y) {
+  line(x, y, x, y + 100);
+  line(x, y, x + 40, y);
+  line(x + 40, y, x + 40, y + 100);
+  line(x, y + 100, x + 40, y + 100);
+  line(x + 30, y + 50, x + 30, y + 110);
+}
+
+function printR(x, y) {
+  line(x, y, x, y + 100);
+  line(x + 40, y, x + 40, y + 50);
+  line(x, y + 50, x + 40, y + 50);
+  line(x, y, x + 40, y);
+  line(x + 30, y + 50, x + 30, y + 100);
+  line(x + 30, y + 100, x + 40, y + 100);
+}
+
+function printS(x, y) {
+  line(x, y, x, y + 50);
+  line(x, y, x + 40, y);
+  line(x, y + 50, x + 40, y + 50);
+  line(x, y + 100, x + 40, y + 100);
+  line(x + 40, y + 50, x + 40, y + 100);
+}
+
+function printT(x, y) {
+  line(x + 20, y, x + 20, y + 100);
+  line(x, y, x + 40, y);
+}
+
+function printU(x, y) {
+  line(x, y, x, y + 100);
+  line(x + 40, y, x + 40, y + 100);
+  line(x, y + 100, x + 40, y + 100);
+}
+
+function printV(x, y) {
+  line(x, y, x, y + 50);
+  line(x + 40, y, x + 40, y + 50);
+  line(x + 10, y + 100, x + 30, y + 100);
+  line(x, y + 50, x + 10, y + 50);
+  line(x + 30, y + 50, x + 40, y + 50);
+  line(x + 10, y + 50, x + 10, y + 100);
+  line(x + 30, y + 50, x + 30, y + 100);
+}
+
+function printW(x, y) {
+  line(x, y, x, y + 100);
+  line(x + 40, y, x + 40, y + 100);
+  line(x + 20, y, x + 20, y + 100);
+  line(x, y + 100, x + 40, y + 100);
+}
+
+function printX(x, y) {
+  line(x, y, x + 10, y);
+  line(x + 10, y, x + 10, y + 50);
+  line(x, y + 50, x, y + 100);
+  line(x, y + 50, x + 40, y + 50);
+  line(x + 40, y, x + 40, y + 50);
+  line(x + 30, y + 50, x + 30, y + 100);
+  line(x + 30, y + 100, x + 40, y + 100);
+}
+
+function printY(x, y) {
+  line(x, y, x, y + 50);
+  line(x + 40, y, x + 40, y + 50);
+  line(x, y + 50, x + 40, y + 50);
+  line(x + 20, y + 50, x + 20, y + 100);
+}
+
+function printZ(x, y) {
+  line(x+30, y, x+30, y + 50);
+  line(x, y, x + 40, y);
+  line(x+10, y + 50, x + 30, y + 50);
+  line(x, y + 100, x + 40, y + 100);
+  line(x + 10, y + 50, x + 10, y + 100);
+}
+
+function draw() {
+  background(220);
+  printH(50, 50);
+  printE(100, 50);
+  printY(150, 50);
+
+  printT(50, 180);
+  printR(100, 180);
+  printY(150, 180);
+ 
+  printT(50, 310);
+  printO(100, 310);
+
+  printF(50, 440);
+  printI(100, 440);
+  printN(150, 440);
+  printD(200, 440);
+
+  printM(50, 570);
+  printE(100, 570);
+}
